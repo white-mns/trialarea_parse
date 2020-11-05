45d1d0c35b21b6319802590287e66d35bde1988c
shiro <shiro.mns@gmail.com>
2020-11-05 21:57:46 +0900

    [追加]正規表現でのノード検索

--- source/lib/GetNode.pm
+++ source/lib/GetNode.pm
@@ -12,6 +12,36 @@ use HTML::TreeBuilder;
 #-----------------------------------#
 #    引数｜タグ名、属性名、条件
 #-----------------------------------#
+sub GetNode_Tag_Attr_RegExp {
+    my $tag_name   = shift;
+    my $attr_name  = shift;
+    my $attr_value = shift;
+    my $node       = shift;
+
+    my $return_nodes    = [];
+
+    if (!$$node) {
+        print "Warning: Use of uninitialized node [GetNode_Tag_Attr_RegExp:$tag_name, $attr_name, $attr_value]\n";
+        return $return_nodes;
+    }
+
+    #各メニュー情報の抜出
+    @$return_nodes = $$node->look_down(
+                _tag => $tag_name,
+                sub {
+                    if( $_[0]->attr($attr_name)){
+                        $_[0]->attr($attr_name) =~ $attr_value
+                    }
+                }
+    );
+    return $return_nodes;
+}
+
+#-----------------------------------#
+#    ノードの取得
+#-----------------------------------#
+#    引数｜タグ名、属性名、条件
+#-----------------------------------#
 sub GetNode_Tag_Attr {
     my $tag_name   = shift;
     my $attr_name  = shift;
