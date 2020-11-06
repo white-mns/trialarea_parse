6a1d01d72e8af5b15eadf5903f1e5d38fa391d55
shiro <shiro.mns@gmail.com>
2020-11-06 16:59:10 +0900

    [追加]試行領域に合わせた処理、結果のダウンロード

--- execute.sh
+++ execute.sh
@@ -4,70 +4,30 @@ CURRENT=`pwd`	#実行ディレクトリの保存
 cd `dirname $0`	#解析コードのあるディレクトリで作業をする
 
 #------------------------------------------------------------------
-# 更新回数、再更新番号の定義確認、設定
+# 大会番号、ラウンド番号の定義確認、設定
 
 
-# 更新回数の指定がない場合は処理しない
-if [ -z "$1" ]; then
+# 大会番号の指定がない場合は処理しない
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "Missing argument."
     exit
 fi
 
-RESULT_NO=`printf "%03d" $1`
-GENERATE_NO=$2
-
-# 再更新番号の指定がない場合、取得済みで最も再更新番号の大きいファイルを探索して実行する
-if [ -z "$2" ]; then
-    for ((GENERATE_NO=5;GENERATE_NO >=0;GENERATE_NO--)) {
-        
-        if [ $GENERATE_NO -eq 0 ]; then
-            ZIP_NAME=${RESULT_NO}
-        else
-            ZIP_NAME=${RESULT_NO}_$GENERATE_NO
-        fi
-
-        echo "test $ZIP_NAME"
-        if [ -f ./data/utf/${ZIP_NAME}.zip ]; then
-            echo "execute $ZIP_NAME"
-            break
-        fi
-    }
-fi
-
-if [ $GENERATE_NO -eq 0 ]; then
-    ZIP_NAME=${RESULT_NO}
-else
-    ZIP_NAME=${RESULT_NO}_$GENERATE_NO
-fi
+EXECUTE_WGET=$1
+RESULT_NO=$2
+RESULT_ADDR_NO=$(($2 + 1))
+ROUND_NO=$3
 
 #------------------------------------------------------------------
-
-# 元ファイルを変換し圧縮
-if [ -f ./data/utf/${ZIP_NAME}.zip ]; then
-    
-    cd ./data/utf
-
-    echo "unzip orig..."
-    unzip -q ./${ZIP_NAME}.zip
-    
-    cd ../../
-
+# 結果に各個アクセスするシェルスクリプトを実行
+if [ $EXECUTE_WGET -eq 1 ]; then
+    ./_result_download.sh $RESULT_NO $ROUND_NO
 fi
+#------------------------------------------------------------------
 
-# 圧縮ファイルが展開されていれば処理の実行
-if [ -d ./data/utf/${ZIP_NAME} ]; then
-    
-    # 解析処理の実行
-    perl ./GetData.pl $1 $GENERATE_NO
-    perl ./UploadParent.pl $1 $GENERATE_NO
-    
-    # UTFファイルを圧縮
-    cd ./data/utf/
+# 解析処理の実行
+perl ./GetData.pl $RESULT_NO $ROUND_NO $GENERATE_NO
+perl ./UploadParent.pl $RESULT_NO $ROUND_NO $GENERATE_NO
 
-    echo "rm utf..."
-    rm  -r ${ZIP_NAME}
-        
-    cd ../../
-
-fi
 
 cd $CURRENT  #元のディレクトリに戻る
